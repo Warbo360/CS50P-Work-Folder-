979540c8b560b3e5b3fb92ffe871863424bc37bf
Warbo360 <129965715+Warbo360@users.noreply.github.com>
2024-08-02 02:20:30 -0500

    Main func that handles args len and file types of args

---
+++ shirt/shirt.py
@@ -0,0 +1,23 @@
+import sys
+#import Pillow
+
+def main():
+    if len(sys.argv) < 3:
+        sys.exit('Too few args')
+    elif len(sys.argv) > 3:
+        sys.exit('Too many args')
+    elif '.jpg' not in sys.argv[1] and '.jpeg' not in sys.argv[1] and '.png' not in sys.argv[1]:
+        sys.exit('Incorrect file type')
+    elif sys.argv[1][-3:] != sys.argv[2][-3:]:
+        sys.exit('Args must be same file type')
+    else:
+        edit(sys.argv[1], sys.argv[2]) # CLI args added after calling program in CLI [1] to be file to read, [2] to be the write file
+
+#def edit(input, output):
+#    try:
+#    #some chunk of code that takes input pic and input shirt pic and edits shirt in place of person
+#    except FileNotFoundError:
+#        sys.exit('File does not exist')
+
+if __name__ == '__main__':
+    main()
