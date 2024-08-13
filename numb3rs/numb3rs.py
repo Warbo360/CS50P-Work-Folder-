e8c0bc865c9bf90f57fa2189174efc9072fdb262
walter-betz <wbetz360@gmail.com>
2024-08-13 16:26:15 -0500

    Made return values booleans

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -10,9 +10,9 @@ def main():
 
 def validate(ip):
     if re.search(r'^(([1]?[0-9]?[0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([1]?[0-9]?[0-9]|[2][0-4][0-9]|[2][5][0-5])$', ip):
-        return f'True'
+        return True
     else:
-        return f'False'
+        return False
 
 if __name__ == '__main__':
     main()
--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -15,40 +15,40 @@ def test_validate():
     test_validate_digit_05()
 
 def test_validate_alpha():
-    assert validate('A.A.A.A') == 'False'
+    assert validate('A.A.A.A') == False
 
 def test_validate_num():
-    assert validate('12.12.12.12') == 'True'
+    assert validate('12.12.12.12') == True
 
 def test_validate_white_space():
-    assert validate('     12.12.12.12         ') == 'False'
+    assert validate('     12.12.12.12         ') == False
 
 def test_validate_wrong_reps():
-    assert validate('12.12.12') == 'False'
+    assert validate('12.12.12') == False
 
 def test_validate_wrong_num_range_total():
-    assert validate('400.400.400.400') == 'False'
+    assert validate('400.400.400.400') == False
 
 def test_validate_wrong_num_range_one_rep():
-    assert validate('400.12.12.12') == 'False'
+    assert validate('400.12.12.12') == False
 
 def test_validate_dot_delimit():
-    assert validate('40012.12.12') == 'False'
+    assert validate('40012.12.12') == False
 
 def test_validate_digit_01():
-    assert validate('012.054.111.009') == 'False'
+    assert validate('012.054.111.009') == False
 
 def test_validate_digit_02():
-    assert validate('012.54.111.9') == 'False'
+    assert validate('012.54.111.9') == False
 
 def test_validate_digit_03():
-    assert validate('12.054.111.9') == 'False'
+    assert validate('12.054.111.9') == False
 
 def test_validate_digit_04():
-    assert validate('12.54.111.009') == 'False'
+    assert validate('12.54.111.009') == False
 
 def test_validate_digit_05():
-    assert validate('12.54.111.9') == 'True'
+    assert validate('12.54.111.9') == True
 
 if __name__ == '__test_validate__':
     test_validate()
