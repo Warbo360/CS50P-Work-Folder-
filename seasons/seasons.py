ecb48f8c1e65347be09277c7c34d5dc69d8c2f66
walter-betz <wbetz360@gmail.com>
2024-08-23 14:26:11 -0500

    got rid of print statments used for testing in seasons.py

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -8,13 +8,11 @@ def main():
     print(get_age(input('Date of Birth: ')))
 
 def get_age(birth_day):
-    print(date.today())
     try:
         age = date.today()-date.fromisoformat(birth_day)
     except ValueError:
         sys.exit('Invalid date, input as YYYY-MM-DD')
     else:
-        print(age)
         return f'{(p.number_to_words(int(age.total_seconds()/60), andword="")).capitalize()} minutes'
 
 if __name__ == "__main__":
