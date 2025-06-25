a9bad564de00c14d20b6520d503323f168537ef6
wbetz360 <129965715+Warbo360@users.noreply.github.com>
2025-06-24 19:34:53 -0500

    updated the indicated file to adhere to lsp quick fixes

--- test_plates/plates.py
+++ test_plates/plates.py
@@ -5,24 +5,30 @@ def main():
     else:
         print('Invalid')
 
+
 def is_valid(s):
-    if middle_check(s) == True and length_check(s) == True and start_is_alpha(s) == True and s.isalnum():
+    if (middle_check(s)
+        and length_check(s)
+        and start_is_alpha(s)
+            and s.isalnum()):
         return True
     else:
         return False
 
+
 def middle_check(m):
     for c in m[2:]:
         if c.isnumeric():
-            _,b,d = m.partition(c)
+            _, b, d = m.partition(c)
             if b == '0':
                 return False
-            elif d.isnumeric() == False:
+            elif d.isnumeric():
                 return False
             else:
                 return True
         else:
             return True
+
 
 def length_check(s):
     if 2 <= len(s) <= 6:
@@ -30,12 +36,13 @@ def length_check(s):
     else:
         return False
 
+
 def start_is_alpha(s):
     for c in s[0:2]:
         if not c.isalpha():
             return False
     return True
 
+
 if __name__ == '__main__':
     main()
-
