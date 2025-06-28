48bd8473db390c64811367b734a82bcfd2c0c601
Walter Betz <129965715+Warbo360@users.noreply.github.com>
2025-06-28 14:32:55 -0500

    Playing around with indexing into rows then colums

--- practiceWithXlsx.py
+++ practiceWithXlsx.py
@@ -1,25 +1,28 @@
-from openpyxl import load_workbook
+from openpyxl import load_workbook  # Allows reading and writing speadsheet files
 import sys
 import re
 
 
 def main():
-    get_ws()
-    wb = load_workbook(filename=sys.argv[1])
-    ws = wb.active
+    ws = get_ws(sys.argv[1])
+    for rows in ws.values:
+        for ind in range(len(rows)):
+            if rows[ind] == "Sample ID":
+                print(rows)
+    rows = ws.values
     APQL = get_APQL(ws)
     num_of_samples = get_sample_num(ws)
     A_num = get_A_num(ws)
-    i = 0
-    while i < num_of_samples:
-        print(f'''
-              Sample ID XXXXXXXXXX XXXX
-              Appearance & Color: Clear and Colorless
-              {A_num}: Not detected. Reported as "<{APQL} ug/mL (APQL).".
-              Other Peak(s): Not detected.
-              Total({A_num} + Other Peak(s)): Reported as "<{APQL} ug/mL (APQL).".
-              ''')
-        i = i + 1
+    # i = 0
+    # while i < num_of_samples:
+    #     print(f'''
+    #           Sample ID XXXXXXXXXX XXXX
+    #           Appearance & Color: Clear and Colorless
+    #           {A_num}: Not detected. Reported as "<{APQL} ug/mL (APQL).".
+    #           Other Peak(s): Not detected.
+    #           Total({A_num} + Other Peak(s)): Reported as "<{APQL} ug/mL (APQL).".
+    #           ''')
+    #     i = i + 1
 
 
 def get_APQL(worksheet):
@@ -47,12 +50,14 @@ def get_A_num(worksheet):
 
 
 def get_ws(filename):
-    if "*.xlsx" in filename:
+    if re.search('.+.xlsx+', filename):
         try:
-            wb = load_workbook(filename=sys.argv[1]) # Spreadsheet should only have one sheet
+            wb = load_workbook(filename=sys.argv[1])  # Spreadsheet should only have one sheet
             return wb.active
         except FileNotFoundError:
-            sys.exit('File not found!')
+            sys.exit('File not Found!')
+    else:
+        sys.exit('File is not an ".xlsx" file')
 
 
 main()
