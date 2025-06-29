0e7205cb71367e8f291b42ffda2317ee49e2cf20
wbetz360 <129965715+Warbo360@users.noreply.github.com>
2025-06-29 10:41:44 -0500

    Reworked funcs of practice, will rename as final project. Added dedicated function that gathers sample data and arranges into a list of dicts

--- practiceWithXlsx.py
+++ practiceWithXlsx.py
@@ -3,65 +3,69 @@ import sys
 import re
 
 
-samples = []
-
-
 def main():
     ws = get_ws(sys.argv[1])
-    # APQL = get_APQL(ws)
-    # num_of_samples = get_sample_num(ws)
-    # A_num = get_A_num(ws)
+    APQL = get_APQL(ws)
+    num_of_samples = get_sample_num(ws)
+    A_num = get_A_num(ws)
+    swabs = determine_swabs(ws)
     # statement_gen(num_of_samples, APQL, A_num)
-    for rows in ws.iter_rows(min_row=11, max_col=6, values_only=True):
-        # print(rows)
-        # print(rows[0])
-        samples.append({
+
+
+def statement_gen(sample_number, limit, analyte, sample_list):
+    i = 0
+    while i < sample_number:
+        i = i + 1
+
+
+# Gathers sample data and arranges it into a list of dicts
+def sample_info_gatherer(worksheet):
+    sample_list = []
+    for rows in worksheet.iter_rows(min_row=14, max_col=6, values_only=True):
+        sample_list.append({
             'Sample ID': rows[0],
             'Sample Type': rows[1],
             'Material': rows[2],
             'Appearance': rows[3],
             'A-Result': rows[4],
             'Other-Peak(s)': rows[5]
-        })
-    print(samples[0])
+            })
+    return sample_list
 
 
-def statement_gen(sample_number, limit, analyte):
-    i = 0
-    while i < sample_number:
-        print('''
-              Sample ID XXXXXXXXXX XXXX
-              Appearance & Color: Clear and Colorless
-              {analyte}: Not detected. Reported as "<{limit} ug/mL (APQL).".
-              Other Peak(s): Not detected.
-              Total({analyte} + Other Peak(s)): Reported as "<{limit} ug/mL (APQL).".''')
-        i = i + 1
-
-
+# Gets the value of APQL
 def get_APQL(worksheet):
-    for rows in worksheet.values:
-        for ind in range(len(rows)):
-            if rows[ind] == "APQL:":
-                return float(rows[ind+1])
+    if isinstance(worksheet['B6'].value, float):
+        return worksheet['B6']
+    elif isinstance(worksheet['B6'].value, int):
+        return worksheet['B6']
+    else:
+        sys.exit('APQL is not set to a numerical value. Please fix and try again.')
 
 
+# Gets the number of samples
 def get_sample_num(worksheet):
-    for rows in worksheet.values:
-        for ind in range(len(rows)):
-            if rows[ind] == "Sample #:":
-                return float(rows[ind+1])
+    try:
+        if worksheet['B7'].value.is_integer():
+            return worksheet['B7'].value
+        else:
+            sys.exit('Sample number is not set as an integer. Please fix and try again')
+    except AttributeError:
+        sys.exit('Sample number has non-numeric characters. Please fix and try again.')
 
 
+# Gets name of target analyte
 def get_A_num(worksheet):
-    for rows in worksheet.values:
-        for ind in range(len(rows)):
-            if rows[ind] == "Target-Analyte":
-                try:
-                    return rows[ind+1]
-                except ValueError:
-                    sys.exit('Target-Analyte is not a string value')
+    try:
+        if worksheet['B5'].value:
+            return worksheet['B5'].value
+        else:
+            sys.exit('Target Analyte is not set. Please fix and try again.')
+    except IOError:
+        sys.exit('Worksheet Error')
 
 
+# Gets the active Worksheet from input Workbook
 def get_ws(filename):
     if re.search('.+.xlsx+', filename):
         try:
@@ -71,6 +75,17 @@ def get_ws(filename):
             sys.exit('File not Found!')
     else:
         sys.exit('File is not an ".xlsx" file')
+
+
+# Determines if sample set are swabs or not
+def determine_swabs(worksheet):
+    try:
+        if worksheet['B8'].value.strip().lower() == 'yes':
+            return True
+        else:
+            return False
+    except AttributeError:
+        sys.exit('Swabs in Cell "B8" not set to yes or no. Please fix and try again')
 
 
 main()
