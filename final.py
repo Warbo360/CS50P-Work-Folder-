399562ff311528debd637d30aad15b2dbd1c52e0
Walter Betz <129965715+Warbo360@users.noreply.github.com>
2025-07-02 21:20:59 -0500

    Formatting fix of both lib and final.py from what the LSP gave

--- final.py
+++ final.py
@@ -1,33 +1,150 @@
+from openpyxl import load_workbook  # Allows reading and writing speadsheet files
 import sys
-from openpyxl import load_workbook
-
-
-wb = load_workbook(filename='empty_book.xlsx', read_only=True)
-# Practice using openpyxl to access Excel file
+import re
+from statement_gen_lib import *
 
 
 def main():
-    if len(sys.argv) == 2:
-        auto_gen((results_pull(sys.argv[1])))
-    elif len(sys.argv) < 2:
-        manual_gen()
+    ws = get_ws(sys.argv[1])
+    statement_gen_rinse(sample_set_org(determine_swabs(ws),
+                                       get_swab_APQL(ws),
+                                       get_A_num(ws),
+                                       get_APQL(ws),
+                                       get_sample_data(ws)))
+
+
+# TODO: Finish writing statment generator function
+def statement_gen_rinse(sample_set_dict):
+    if isinstance(sample_set_dict['Limit'], (int, float)):
+        rinse_sample_gen(sample_set_dict)
+    elif isinstance(sample_set_dict['Limit'], (dict)):
+        # TODO: Make a swab sample statement generator!
+        swab_sample_gen(sample_set_dict)
+
+
+# Makes a dict out of relevant sample set info
+def sample_set_org(swab_state, swab_limits, analyte, limit, sample_list):
+    sample_set_info = {}
+    if swab_state:
+        sample_set_info['Limit'] = swab_limits
     else:
-        sys.exit('Too many arguments')
-
-# Will pull results straight from file in CLI args
-
-
-def results_pull(file):
-    return (file)
+        sample_set_info['Limit'] = limit
+    sample_set_info['Analyte'] = analyte
+    sample_set_info['Sample List'] = sample_list
+    return sample_set_info
 
 
-def auto_gen(file_test):
-    print(file_test)
+# Gather Swab APQL info return that info as a list of dicts
+def get_swab_APQL(worksheet):
+    material_APQLs = []
+    for rows in worksheet.iter_rows(min_row=2, max_row=9, min_col=4, max_col=5, values_only=True):
+        if rows[0] and isinstance(rows[1], (int, float)):
+            material_APQLs.append({
+                'Material': rows[0],
+                'APQL': rows[1]
+                })
+        elif not rows[0] and not rows[1]:
+            continue
+        else:
+            print(f'Please check the row of the {rows[0]} entry and that its inforamation is correct')
+    return material_APQLs
 
 
-def manual_gen():
-    print('Make your own results')
+# Gathers sample data and arranges it into a list of dicts, also automatically converts 'Other-Peak(s)' into one float
+def get_sample_data(worksheet):
+    sample_list = []
+    for rows in worksheet.iter_rows(min_row=14, max_col=6, values_only=True):
+        if (rows[0] and
+                rows[1]):
+            sample_list.append({
+                'Sample ID': rows[0],
+                'Sample Type': rows[1],
+                'Material': rows[2],
+                'Appearance': rows[3],
+                'A-Result': rows[4],
+                'Other-Peak(s)': rows[5],
+                })
+    for dicts in sample_list:
+        if isinstance(dicts['Other-Peak(s)'], (int, float)):
+            dicts['Summed-Other-Peak(s)'] = dicts['Other-Peak(s)']
+            try:
+                dicts['Summed-Total-Peak(s)'] = dicts['Other-Peak(s)'] + dicts['A-Result']
+            except TypeError:
+                sys.exit('An "A-Result" entry is non-numerical. Please fix and try again.')
+        elif not dicts['Other-Peak(s)']:
+            dicts['Summed-Other-Peak(s)'] = dicts['Other-Peak(s)']
+            try:
+                dicts['Summed-Total-Peak(s)'] = dicts['A-Result']
+            except TypeError:
+                sys.exit('An "A-Result" entry is non-numerical. Please fix and try again.')
+        else:
+            try:
+                other_peaks = dicts['Other-Peak(s)'].split(',')
+            except TypeError:
+                sys.exit('One of more "Other-Peak(s)" entries are non-numerical values, please fix and try again')
+            sum_of_peaks = 0
+            for peaks in other_peaks:
+                peaks = float(peaks)
+                sum_of_peaks = sum_of_peaks + peaks
+            sum_of_total_peaks = sum_of_peaks + float(dicts['A-Result'])
+            dicts['Summed-Other-Peak(s)'] = sum_of_peaks
+            dicts['Summed-Total-Peak(s)'] = sum_of_total_peaks
+    return sample_list
 
 
-if __name__ == "__main__":
-    main()
+# Gets the value of APQL
+def get_APQL(worksheet):
+    if isinstance(worksheet['B6'].value, float):
+        return worksheet['B6'].value
+    elif isinstance(worksheet['B6'].value, int):
+        return worksheet['B6'].value
+    else:
+        sys.exit('APQL is not set to a numerical value. Please fix and try again.')
+
+
+# Gets the number of samples
+def get_sample_num(worksheet):
+    try:
+        if worksheet['B7'].value.is_integer():
+            return worksheet['B7'].value
+        else:
+            sys.exit('Sample number is not set as an integer. Please fix and try again')
+    except AttributeError:
+        sys.exit('Sample number has non-numeric characters. Please fix and try again.')
+
+
+# Gets name of target analyte
+def get_A_num(worksheet):
+    try:
+        if worksheet['B5'].value:
+            return worksheet['B5'].value
+        else:
+            sys.exit('Target Analyte is not set. Please fix and try again.')
+    except IOError:
+        sys.exit('Worksheet Error')
+
+
+# Gets the active Worksheet from input Workbook
+def get_ws(filename):
+    if re.search('.+.xlsx+', filename):
+        try:
+            wb = load_workbook(filename=sys.argv[1])  # Spreadsheet should only have one sheet
+            return wb.active
+        except FileNotFoundError:
+            sys.exit('File not Found!')
+    else:
+        sys.exit('File is not an ".xlsx" file')
+
+
+# Determines if sample set are swabs or not
+def determine_swabs(worksheet):
+    try:
+        if worksheet['B8'].value.strip().lower() == 'yes':
+            return True
+        else:
+            return False
+    except AttributeError:
+        sys.exit('Swabs in Cell "B8" not set to yes or no. Please fix and try again')
+
+
+main()
--- statement_gen_lib.py
+++ statement_gen_lib.py
@@ -5,67 +5,65 @@
 # then comparative logic that points each entru to its associated statment generator
 def rinse_sample_gen(sample_list):
     for dicts in sample_list['Sample List']:
-# Permutation of all passing blank sample
         if (dicts['Sample Type'].lower().strip() == 'blank' and
-        dicts['Appearance'].lower().strip() == 'pass' and
-        isinstance(sample_list['Limit'], (int, float)) and
-        dicts['A-Result'] < sample_list['Limit']):
+            dicts['Appearance'].lower().strip() == 'pass' and
+            isinstance(sample_list['Limit'], (int, float)) and
+                dicts['A-Result'] < sample_list['Limit']):
             passing_rinse_blank(sample_list, dicts)
 # Permutation of all passing except appearance for blank rinse sample
         elif (dicts['Sample Type'].lower().strip() == 'blank' and
-        dicts['Appearance'].lower().strip() == 'fail' and
-        isinstance(sample_list['Limit'], (int, float)) and
-        dicts['A-Result'] < sample_list['Limit']):
+                dicts['Appearance'].lower().strip() == 'fail' and
+                isinstance(sample_list['Limit'], (int, float)) and
+                dicts['A-Result'] < sample_list['Limit']):
             pass_fail_rinse_blank(sample_list, dicts)
 # Permutation of failing blank but passing appearance
         elif (dicts['Sample Type'].lower().strip() == 'blank' and
-        dicts['Appearance'].lower().strip() == 'pass' and
-        isinstance(sample_list['Limit'], (int, float)) and
-        dicts['A-Result'] > sample_list['Limit']):
+                dicts['Appearance'].lower().strip() == 'pass' and
+                isinstance(sample_list['Limit'], (int, float)) and
+                dicts['A-Result'] > sample_list['Limit']):
             fail_pass_rinse_blank(sample_list, dicts)
 # Permuation of failing appearance and A Result for blanks
         elif (dicts['Sample Type'].lower().strip() == 'blank' and
-        dicts['Appearance'].lower().strip() == 'fail' and
-        isinstance(sample_list['Limit'], (int, float)) and
-        dicts['A-Result'] > sample_list['Limit']):
+                dicts['Appearance'].lower().strip() == 'fail' and
+                isinstance(sample_list['Limit'], (int, float)) and
+                dicts['A-Result'] > sample_list['Limit']):
             fail_rinse_blank(sample_list, dicts)
 # Permuation of all passing normal rinse samp with A-Result not detected (excel cell = 0)
         elif (dicts['Sample Type'].lower().strip() == 'sample' and
-        dicts['Appearance'].lower().strip() == 'pass' and
-        isinstance(sample_list['Limit'], (int, float)) and
-        dicts['A-Result'] == 0):
+                dicts['Appearance'].lower().strip() == 'pass' and
+                isinstance(sample_list['Limit'], (int, float)) and
+                dicts['A-Result'] == 0):
             zero_pass_rinse_sample(sample_list, dicts)
 # Permutation of passing rinse with detected A-result but still less than APQL
         elif (dicts['Sample Type'].lower().strip() == 'sample' and
-        dicts['Appearance'].lower().strip() == 'pass' and
-        isinstance(sample_list['Limit'], (int, float)) and
-        sample_list['Limit'] > dicts['A-Result'] > 0):
+                dicts['Appearance'].lower().strip() == 'pass' and
+                isinstance(sample_list['Limit'], (int, float)) and
+                sample_list['Limit'] > dicts['A-Result'] > 0):
             less_APQL_pass_rinse_sample(sample_list, dicts)
 # Permutation of passing rinse sample with A-result greather than or equal to APQL
         elif (dicts['Sample Type'].lower().strip() == 'sample' and
-        dicts['Appearance'].lower().strip() == 'pass' and
-        isinstance(sample_list['Limit'], (int, float)) and
-        dicts['A-Result'] >= sample_list['Limit']):
+                dicts['Appearance'].lower().strip() == 'pass' and
+                isinstance(sample_list['Limit'], (int, float)) and
+                dicts['A-Result'] >= sample_list['Limit']):
             greater_APQL_pass_rinse_sample(sample_list, dicts)
 # Permutation of failng Appearance and AR not detected rinse sample
         elif (dicts['Sample Type'].lower().strip() == 'sample' and
-        dicts['Appearance'].lower().strip() == 'fail' and
-        isinstance(sample_list['Limit'], (int, float)) and
-        dicts['A-Result'] == 0):
+                dicts['Appearance'].lower().strip() == 'fail' and
+                isinstance(sample_list['Limit'], (int, float)) and
+                dicts['A-Result'] == 0):
             zero_fail_rinse_sample(sample_list, dicts)
 # Permuatatuon of failong Appearance and AP < APQL rinse sample
         elif (dicts['Sample Type'].lower().strip() == 'sample' and
-        dicts['Appearance'].lower().strip() == 'fail' and
-        isinstance(sample_list['Limit'], (int, float)) and
-        sample_list['Limit'] > dicts['A-Result'] > 0):
+                dicts['Appearance'].lower().strip() == 'fail' and
+                isinstance(sample_list['Limit'], (int, float)) and
+                sample_list['Limit'] > dicts['A-Result'] > 0):
             less_APQL_fail_rinse_sample(sample_list, dicts)
 # Permutation of failing Appearance and AR >= APQL rinse sample
         elif (dicts['Sample Type'].lower().strip() == 'sample' and
-        dicts['Appearance'].lower().strip() == 'fail' and
-        isinstance(sample_list['Limit'], (int, float)) and
-        dicts['A-Result'] >= sample_list['Limit']):
+                dicts['Appearance'].lower().strip() == 'fail' and
+                isinstance(sample_list['Limit'], (int, float)) and
+                dicts['A-Result'] >= sample_list['Limit']):
             greater_APQL_fail_rinse_sample(sample_list, dicts)
-        
 
 
 # Passing rinse blank perm
@@ -95,21 +93,21 @@ Other Peak(s): Not detected. Reported as 'Pass'.''')
 # Passing A-R, Failing Appearance, Rinse Blank
 def pass_fail_rinse_blank(sample_list, dicts):
     if isinstance(dicts['Other-Peak(s)'], (int, float)):
-                print(f'''
+        print(f'''
 {dicts['Sample ID']} Blank
 Appearance and Color: Clear and Colorless. Reported as 'Fail'.
 {sample_list['Analyte']}: Peak not detected. Reported as 'Pass'.
 Other Peak(s): Peak(s) detected at {dicts['Other-Peak(s)']} ug/mL. Reported as 'Pass'.
 Total({sample_list['Analyte']} + Other Peak(s)): {dicts['Summed-Other-Peak(s)']} ug/mL.''')
     elif isinstance(dicts['Other-Peak(s)'], str):
-                print(f'''
+        print(f'''
 {dicts['Sample ID']} Blank
 Appearance and Color: Clear and Colorless. Reported as 'Fail'.
 {sample_list['Analyte']}: Peak not detected. Reported as 'Pass'.
 Other Peak(s): Peak(s) detected at {' ug/mL,'.join(dicts['Other-Peak(s)'].split(','))} ug/mL. Reported as 'Pass'.
 Total({sample_list['Analyte']} + Other Peak(s)): {dicts['Summed-Other-Peak(s)']} ug/mL.''')
     else:
-                print(f'''
+        print(f'''
 {dicts['Sample ID']} Blank
 Appearance and Color: Clear and Colorless. Reported as 'Fail'.
 {sample_list['Analyte']}: Peak not detected. Reported as 'Pass'.
@@ -118,22 +116,22 @@ Other Peak(s): Not detected. Reported as 'Pass'.''')
 
 # Failing AR but passing appearance blank rinse
 def fail_pass_rinse_blank(sample_list, dicts):
-            if isinstance(dicts['Other-Peak(s)'], (int, float)):
-                print(f'''
+    if isinstance(dicts['Other-Peak(s)'], (int, float)):
+        print(f'''
 {dicts['Sample ID']} Blank
 Appearance and Color: Clear and Colorless. Reported as 'Pass'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as 'Fail'.
 Other Peak(s): Peak(s) detected at {dicts['Other-Peak(s)']} ug/mL. Reported as 'Fail'.
 Total({sample_list['Analyte']} + Other Peak(s)): {dicts['Summed-Other-Peak(s)']} ug/mL.''')
-            elif isinstance(dicts['Other-Peak(s)'], str):
-                print(f'''
+    elif isinstance(dicts['Other-Peak(s)'], str):
+        print(f'''
 {dicts['Sample ID']} Blank
 Appearance and Color: Clear and Colorless. Reported as 'Pass'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as 'Fail'.
 Other Peak(s): Peak(s) detected at {' ug/mL,'.join(dicts['Other-Peak(s)'].split(','))} ug/mL. Reported as 'Fail'.
 Total({sample_list['Analyte']} + Other Peak(s)): {dicts['Summed-Total-Peak(s)']} ug/mL.''')
-            else:
-                print(f'''
+    else:
+        print(f'''
 {dicts['Sample ID']} Blank
 Appearance and Color: Clear and Colorless. Reported as 'Pass'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as 'Fail'.
@@ -144,21 +142,21 @@ Total({sample_list['Analyte']} + Other Peak(s)): {dicts['A-Result']} ug/mL.''')
 # Failing AR and appearance for rinse blanks
 def fail_rinse_blank(sample_list, dicts):
     if isinstance(dicts['Other-Peak(s)'], (int, float)):
-                print(f'''
+        print(f'''
 {dicts['Sample ID']} Blank
 Appearance and Color: Clear and Colorless. Reported as 'Fail'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as 'Fail'.
 Other Peak(s): Peak(s) detected at {dicts['Other-Peak(s)']} ug/mL. Reported as 'Fail'.
 Total({sample_list['Analyte']} + Other Peak(s)): {dicts['Summed-Other-Peak(s)']} ug/mL.''')
     elif isinstance(dicts['Other-Peak(s)'], str):
-                print(f'''
+        print(f'''
 {dicts['Sample ID']} Blank
 Appearance and Color: Clear and Colorless. Reported as 'Fail'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as 'Fail'.
 Other Peak(s): Peak(s) detected at {' ug/mL,'.join(dicts['Other-Peak(s)'].split(','))} ug/mL. Reported as 'Fail'.
 Total({sample_list['Analyte']} + Other Peak(s)): {dicts['Summed-Total-Peak(s)']} ug/mL.''')
     else:
-                print(f'''
+        print(f'''
 {dicts['Sample ID']} Blank
 Appearance and Color: Clear and Colorless. Reported as 'Fail'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as 'Fail'.
@@ -170,21 +168,21 @@ Total({sample_list['Analyte']} + Other Peak(s)): {dicts['A-Result']} ug/mL.
 # Zero AR and passing appearance generator for rinse samples
 def zero_pass_rinse_sample(sample_list, dicts):
     if isinstance(dicts['Other-Peak(s)'], (int, float)):
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: Clear and not more intensely colored than the blank sample(s). Reported as 'Pass'.
 {sample_list['Analyte']}: No peak detected. Reported as '< {sample_list['Limit']} ug/mL (<APQL)'.
 Other Peak(s): Peak(s) detected at {dicts['Other-Peak(s)']} ug/mL.
 Total({sample_list['Analyte']} + Other Peak(s)): {dicts['Summed-Other-Peak(s)']} ug/mL.''')
     elif isinstance(dicts['Other-Peak(s)'], str):
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: Clear and not more intensely colored than the blank sample(s). Reported as 'Pass'.
 {sample_list['Analyte']}: No peak detected. Reported as '< {sample_list['Limit']} ug/mL (<APQL)'.
 Other Peak(s): Peak(s) detected at {' ug/mL,'.join(dicts['Other-Peak(s)'].split(','))} ug/mL.
 Total({sample_list['Analyte']} + Other Peak(s)): {dicts['Summed-Total-Peak(s)']} ug/mL.''')
     else:
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: Clear and not more intensely colored than the blank sample(s). Reported as 'Pass'.
 {sample_list['Analyte']}: No peak detected. Reported as '< {sample_list['Limit']} ug/mL (<APQL)'.
@@ -195,21 +193,21 @@ Total({sample_list['Analyte']} + Other Peak(s)): {dicts['A-Result']} ug/mL.''')
 # < APQL and passing appearance generator for risne samples
 def less_APQL_pass_rinse_sample(sample_list, dicts):
     if isinstance(dicts['Other-Peak(s)'], (int, float)):
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: Clear and not more intensely colored than the blank sample(s). Reported as 'Pass'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as '< {sample_list['Limit']} ug/mL (<APQL)'.
 Other Peak(s): Peak(s) detected at {dicts['Other-Peak(s)']} ug/mL.
 Total({sample_list['Analyte']} + Other Peak(s)): < {dicts['Summed-Other-Peak(s)']+sample_list['Limit']} ug/mL.''')
     elif isinstance(dicts['Other-Peak(s)'], str):
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: Clear and not more intensely colored than the blank sample(s). Reported as 'Pass'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as '< {sample_list['Limit']} ug/mL (<APQL)'.
 Other Peak(s): Peak(s) detected at {' ug/mL,'.join(dicts['Other-Peak(s)'].split(','))} ug/mL.
 Total({sample_list['Analyte']} + Other Peak(s)): < {dicts['Summed-Total-Peak(s)']} ug/mL.''')
     else:
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: Clear and not more intensely colored than the blank sample(s). Reported as 'Pass'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as '< {sample_list['Limit']} ug/mL (<APQL)'.
@@ -220,21 +218,21 @@ Total({sample_list['Analyte']} + Other Peak(s)): < {sample_list['Limit']} ug/mL 
 # >= APQL and passing appearance generator for rinse samples
 def greater_APQL_pass_rinse_sample(sample_list, dicts):
     if isinstance(dicts['Other-Peak(s)'], (int, float)):
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: Clear and not more intensely colored than the blank sample(s). Reported as 'Pass'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as '{dicts['A-Result']} ug/mL'.
 Other Peak(s): Peak(s) detected at {dicts['Other-Peak(s)']} ug/mL.
 Total({sample_list['Analyte']} + Other Peak(s)): {dicts['Summed-Other-Peak(s)']+dicts['A-Result']} ug/mL.''')
     elif isinstance(dicts['Other-Peak(s)'], str):
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: Clear and not more intensely colored than the blank sample(s). Reported as 'Pass'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as '{dicts['A-Result']} ug/mL'.
 Other Peak(s): Peak(s) detected at {' ug/mL,'.join(dicts['Other-Peak(s)'].split(','))} ug/mL.
 Total({sample_list['Analyte']} + Other Peak(s)): {dicts['Summed-Other-Peak(s)']+dicts['A-Result']} ug/mL.''')
     else:
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: Clear and not more intensely colored than the blank sample(s). Reported as 'Pass'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as '{dicts['A-Result']} ug/mL'.
@@ -245,21 +243,21 @@ Total({sample_list['Analyte']} + Other Peak(s)): {dicts['A-Result']} ug/mL.''')
 # Zero AR and failing appearance generator for rinse samples
 def zero_fail_rinse_sample(sample_list, dicts):
     if isinstance(dicts['Other-Peak(s)'], (int, float)):
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: More intensely colored and not clear. Reported as 'Fail'.
 {sample_list['Analyte']}: No peak detected. Reported as '< {sample_list['Limit']} ug/mL (<APQL)'.
 Other Peak(s): Peak(s) detected at {dicts['Other-Peak(s)']} ug/mL.
 Total({sample_list['Analyte']} + Other Peak(s)): < {dicts['Summed-Other-Peak(s)']+sample_list['Limit']} ug/mL.''')
     elif isinstance(dicts['Other-Peak(s)'], str):
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: More intensely colored and not clear. Reported as 'Fail'.
 {sample_list['Analyte']}: No peak detected. Reported as '< {sample_list['Limit']} ug/mL (<APQL)'.
 Other Peak(s): Peak(s) detected at {' ug/mL,'.join(dicts['Other-Peak(s)'].split(','))} ug/mL.
 Total({sample_list['Analyte']} + Other Peak(s)): < {dicts['Summed-Total-Peak(s)']+sample_list['Limit']} ug/mL.''')
     else:
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: More intensely colored and not clear. Reported as 'Fail'.
 {sample_list['Analyte']}: No peak detected. Reported as '< {sample_list['Limit']} ug/mL (<APQL)'.
@@ -270,21 +268,21 @@ Total({sample_list['Analyte']} + Other Peak(s)): < {sample_list['Limit']} ug/mL 
 # < APQL and failing appearance generator for rinse samples
 def less_APQL_fail_rinse_sample(sample_list, dicts):
     if isinstance(dicts['Other-Peak(s)'], (int, float)):
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: More intensely colored and not clear. Reported as 'Fail'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as '< {sample_list['Limit']} ug/mL (<APQL)'.
 Other Peak(s): Peak(s) detected at {dicts['Other-Peak(s)']} ug/mL.
 Total({sample_list['Analyte']} + Other Peak(s)): < {dicts['Summed-Other-Peak(s)']+sample_list['Limit']} ug/mL.''')
     elif isinstance(dicts['Other-Peak(s)'], str):
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: More intensely colored and not clear. Reported as 'Fail'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as '< {sample_list['Limit']} ug/mL (<APQL)'.
 Other Peak(s): Peak(s) detected at {' ug/mL,'.join(dicts['Other-Peak(s)'].split(','))} ug/mL.
 Total({sample_list['Analyte']} + Other Peak(s)): < {dicts['Summed-Other-Peak(s)']+sample_list['Limit']} ug/mL.''')
     else:
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: More intensely colored and not clear. Reported as 'Fail'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as '< {sample_list['Limit']} ug/mL (<APQL)'.
@@ -295,21 +293,21 @@ Total({sample_list['Analyte']} + Other Peak(s)): < {sample_list['Limit']} ug/mL 
 # >= APQL and failing appearance generator for rinse samples
 def greater_APQL_fail_rinse_sample(sample_list, dicts):
     if isinstance(dicts['Other-Peak(s)'], (int, float)):
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: More intensely colored and not clear. Reported as 'Fail'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as '{dicts['A-Result']} ug/mL'.
 Other Peak(s): Peak(s) detected at {dicts['Other-Peak(s)']} ug/mL.
 Total({sample_list['Analyte']} + Other Peak(s)): {dicts['Summed-Other-Peak(s)']+dicts['A-Result']} ug/mL.''')
     elif isinstance(dicts['Other-Peak(s)'], str):
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: More intensely colored and not clear. Reported as 'Fail'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as '{dicts['A-Result']} ug/mL'.
 Other Peak(s): Peak(s) detected at {' ug/mL,'.join(dicts['Other-Peak(s)'].split(','))} ug/mL.
 Total({sample_list['Analyte']} + Other Peak(s)): {dicts['Summed-Other-Peak(s)']+dicts['A-Result']} ug/mL.''')
     else:
-                print(f'''
+        print(f'''
 {dicts['Sample ID']}
 Appearance and Color: More intensely colored and not clear. Reported as 'Fail'.
 {sample_list['Analyte']}: Peak detected at {dicts['A-Result']} ug/mL. Reported as '{dicts['A-Result']} ug/mL'.
