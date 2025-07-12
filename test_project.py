09a6e786eff4b5045c3441728e65c0adf464bfa5
Walter Betz Jr <129965715+Warbo360@users.noreply.github.com>
2025-07-12 16:47:32 -0500

    Base fucntionality of test_project layed out for filling in

--- test_project.py
+++ test_project.py
@@ -1,10 +1,23 @@
-import pytest
+from project import get_swab_APQL, get_sample_data, get_APQL
 
 
 def main():
+    test_get_swab_APQL()
+    test_get_sample_data()
+    test_get_APQL()
 
 
-def test_
+def test_get_swab_APQL(worksheet):
+    ...
 
 
-main()
+def test_get_sample_data(worksheet):
+    ...
+
+
+def test_get_APQL(worksheet):
+    ...
+
+
+if __name__ == "__main__":
+    main()
